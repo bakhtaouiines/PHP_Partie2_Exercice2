15a885ee946e878ada05654d6c3893d6634677fd
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 15:14:21 +0200

    correction ajout HTML

--- exercice2.2.php
+++ exercice2.2.php
@@ -1,11 +1,25 @@
-<?php
-$isEasy = true;
-if ($isEasy)
-{
-    echo "C'est facile !!";
-}
-else
-{
-    echo "C'est difficile !!!";
-}
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 2.2</title>
+</head>
+<body>
+    <h1>Exercice 2 Partie 2 : Les conditions PHP</h1>
+    <p>
+        <?php
+            $isEasy = true;
+            if ($isEasy)
+            {
+                echo "C'est facile !!";
+            }
+            else
+            {
+                echo "C'est difficile !!!";
+            }
+        ?>
+    </p>
+</body>
+</html>
